a5c3c9604ec8a3287fdbd777db1faeefe55480fb
12942 <12942@LAPTOP-NLKQLU18>
2022-04-21 20:15:16 +0800

    week8 commit

--- Zhou jiafeng0525SkySharkWebApplication/BM/AddFl.aspx.cs
+++ Zhou jiafeng0525SkySharkWebApplication/BM/AddFl.aspx.cs
@@ -15,7 +15,7 @@ namespace Zhou_jiafeng0525SkySharkWebApplication.BM
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
