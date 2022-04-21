ae9e5c772c89d170f982fe2a45e526c621e0ae28
12942 <12942@LAPTOP-NLKQLU18>
2022-04-21 17:05:32 +0800

    week8 commit

--- Zhou jiafeng0525SkySharkWebApplication/ChangePassword.aspx.designer.cs
+++ Zhou jiafeng0525SkySharkWebApplication/ChangePassword.aspx.designer.cs
@@ -7,7 +7,7 @@
 // </自动生成>
 //------------------------------------------------------------------------------
 
-namespace Zhou_jiafeng0525SkySharkWebApplication
+namespace Zhoujiafeng0525SkySharkWebApplication
 {
 
 
